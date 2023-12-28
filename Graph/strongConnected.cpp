d6c0edffb148165d17324aeeb3f96af4bb66227b
Ioana <ioana@dragoss-MacBook-Air.local>
2023-12-28 18:53:50 -0500

    Global variables for kmp and strongly connected

--- Graph/strongConnected.cpp
+++ Graph/strongConnected.cpp
@@ -1,9 +1,9 @@
-vi g[Nmax],stack,viz,low,iss,aux;
+vi g[Nmax],stk,viz,low,iss,aux;
 vector<vi> comp;
-int k,index=1,N,M,x,y;
+int N, k, indx=1;
 void df(int x){
-  viz[x] = index; low[x] = index;
-  stack[++k] = x; iss[x] = 1; ++index;
+  viz[x] = indx; low[x] = indx;
+  stk[++k] = x; iss[x] = 1; ++indx;
   for(auto n : g[x]){
     if(viz[n] == 0){
       df(n); low[x] = min(low[x],low[n]);
@@ -12,14 +12,19 @@ void df(int x){
   if(low[x] == viz[x]){
     aux.clear();
     do {
-      aux.pb(stack[k]); iss[stack[k]] = 0; --k;
-    } while(stack[k+1] != x);
+      aux.pb(stk[k]); iss[stk[k]] = 0; --k;
+    } while(stk[k+1] != x);
     comp.pb(aux);
   }
 }
 void init(){
-  stack.resize(N+10); viz.resize(N+10);
-  iss.resize(N+10); low.resize(N+10);
+  stk.clear(); stk.resize(N+10);
+  viz.clear(); viz.resize(N+10);
+  iss.clear(); iss.resize(N+10);
+  low.clear(); low.resize(N+10);
+  aux.clear();
+  comp.clear();
+  k=0; indx=1;
   for(int i=1;i<=N;++i) {
     if(!viz[i]) df(i);
   }
--- String/kmp.cpp
+++ String/kmp.cpp
@@ -1,11 +1,12 @@
-void make() {
+int nx[Nmax];
+void make(string &s1) {
   int k = -1; nx[0] = -1;
   for(int i=1;i<sz(s1);++i) {
     while(k >= 0 && s1[k+1] != s1[i]) k = nx[k];
     if(s1[k+1] == s1[i]) ++k; nx[i] = k;
   }
 }
-void match() {
+void match(string &s1, string &s2) {
   int k=-1;
   for(int i=0;i<sz(s2);++i) {
     while(k >= 0 && s1[k+1] != s2[i]) k = nx[k];
