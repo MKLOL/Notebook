221d639d70083f4308a1d688f79854bb62c0ec1b
Dragos Ristache <ristachedragos@gmail.com>
2024-01-26 11:42:42 -0500

    Create classUnionFind.cpp

---
+++ Data Structures/classUnionFind.cpp
@@ -0,0 +1,55 @@
+#include <vector>
+
+class UnionFind {
+private:
+    vector<int> par;
+    vector<int> h;
+    int size;
+
+public:
+    // Constructor
+    UnionFind(int size) : size(size) {
+        par.resize(size);
+        h.resize(size, 0);
+        for (int i = 0; i < size; ++i) {
+            par[i] = i;
+        }
+    }
+
+    // Find operation with path compression
+    int find(int x) {
+        int R = x, y;
+        while (par[R] != R) {
+            R = par[R];
+        }
+        while (par[x] != x) {
+            y = par[x];
+            par[x] = R;
+            x = y;
+        }
+        return R;
+    }
+
+    // Union operation by rank
+    void unite(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x == y) return;
+
+        if (h[x] > h[y]) {
+            par[y] = x;
+            h[x] += h[y];
+        } else {
+            par[x] = y;
+            h[y] += h[x];
+        }
+    }
+
+    // Clear operation - reset to initial values
+    void clear() {
+        for (int i = 0; i < size; ++i) {
+            par[i] = i;
+            h[i] = 0;
+        }
+    }
+};
