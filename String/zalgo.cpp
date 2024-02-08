04032ac9a92052bbb8ac36e346e84eea2d16cda2
Dragos Ristache <ristachedragos@gmail.com>
2024-02-08 11:56:28 -0500

    Update zalgo.cpp

--- String/zalgo.cpp
+++ String/zalgo.cpp
@@ -1,14 +1,21 @@
-// z -> int array, s2 -> char array, N, it's length. At the end have z[i]
-int left=0, right=0;
-for(int i=1;i<N;++i) {
-  if( i > right) {
-    left = i, right = i;
-    while (right < N && s2[right-left] == s2[right]) ++right;
-    z[i] = right - left; --right;
-  } else {
-    int k = i - left; if(z[k] < right-i+1) z[i] = z[k];
-    else {
-      left=i;
-      while (right < N && s2[right - left] == s2[right]) ++right;
-      z[i] = right-left; --right;
-}}}
+vi make(const string& s) {
+    int N = s.size();
+    vi z(N, 0); // Initialize z vector with N elements, all set to 0
+    int left = 0, right = 0;
+    for(int i = 1; i < N; ++i) {
+        if(i > right) {
+            left = i, right = i;
+            while (right < N && s[right - left] == s[right]) ++right;
+            z[i] = right - left; --right;
+        } else {
+            int k = i - left;
+            if(z[k] < right - i + 1) z[i] = z[k];
+            else {
+                left = i;
+                while (right < N && s[right - left] == s[right]) ++right;
+                z[i] = right - left; --right;
+            }
+        }
+    }
+    return z;
+}
