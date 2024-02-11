c17a27257bbe320539930e7d812d7e8a45b012fe
Ioana <ioana@dragoss-MacBook-Air.local>
2024-02-10 22:44:25 -0500

    improve kmp

--- String/kmp.cpp
+++ String/kmp.cpp
@@ -1,16 +1,29 @@
-int nx[Nmax];
-void make(string &s1) {
-  int k = -1; nx[0] = -1;
-  for(int i=1;i<sz(s1);++i) {
-    while(k >= 0 && s1[k+1] != s1[i]) k = nx[k];
-    if(s1[k+1] == s1[i]) ++k; nx[i] = k;
+class Kmp {
+private:
+  vi nx;
+public:
+  Kmp(string &s1) {
+    nx.resize(sz(s1));
+    int k = -1; nx[0] = -1;
+    rep(i,1,sz(s1)) {
+      while(k >= 0 && s1[k+1] != s1[i]) k = nx[k];
+      if(s1[k+1] == s1[i]) ++k;
+      nx[i] = k;
+    } 
   }
-}
-void match(string &s1, string &s2) {
-  int k=-1;
-  for(int i=0;i<sz(s2);++i) {
-    while(k >= 0 && s1[k+1] != s2[i]) k = nx[k];
-    if(s1[k+1] == s2[i]) ++k; 
-    if(k==sz(s1)-1) k = nx[k]; // match on i - sz(s1)
+
+  // s1 is pattern, s2 is big string
+  vi match(string &s1, string &s2) {
+    vi ret;
+    int k=-1;
+    for(int i=0;i<sz(s2);++i) {
+      while(k >= 0 && s1[k+1] != s2[i]) k = nx[k];
+      if(s1[k+1] == s2[i]) ++k; 
+      if(k==sz(s1)-1) {
+        k = nx[k];
+        ret.pb(i-sz(s1)+1);
+      }
+    }
+    return ret;
   }
-}
+};
