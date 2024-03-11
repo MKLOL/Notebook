9640e3cf8c9d5ee24c6e0ed33436b688e6c345bc
Dragos Ristache <ristachedragos@gmail.com>
2024-03-11 15:30:05 -0400

    Update pscpld.cpp

--- String/pscpld.cpp
+++ String/pscpld.cpp
@@ -1,26 +1,28 @@
-char s[2010000], s1[2010000];
-int val[2020201],maxind,maxVal,N;
-void make_sir(){
-  s1[0]='*';
-  for(int i=1;i<=N;++i) { s1[i*2-1]=s[i]; s1[i*2]='*';}
-}
-ll pscpld (char *s) {
-  ll S = 0; N = strlen(s+1); make_sir();
-  for(int i=1;i<2*N;++i) {
+vi pscpld (string s) {
+  string s1;
+  int maxind=0,maxVal=0;
+  for (int i=0;i<sz(s);++i) {
+    s1 += s[i];
+    if (i != sz(s)-1) {
+      s1 += "*";
+    }
+  }
+
+  vi val(sz(s1));
+
+  for(int i=0;i<sz(s1);++i) {
     if(maxVal >= i) {
       int loc = maxind - (i-maxind);
       val[i] = min(val[loc],maxVal-i);
     }
-    while((i - val[i] >= 0) && (i + val[i] <= 2*N) && 
+    while((i - val[i] >= 0) && (i + val[i] < sz(s1)) && 
           (s1[i-val[i]] == s1[i+val[i]])) {
       ++val[i];
       if(i + val[i] > maxVal){
-        maxVal = i + val[i]; maxind = i;
+        maxVal = i + val[i] - 1; maxind = i;
       }
     }
   }
-  for(int i=1;i<2*N;++i){
-    S += (val[i]+1)/2; if(s1[i]=='*') --S;
-  }
-  return S;
+  
+  return val;
 }
