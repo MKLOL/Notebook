af0eb68a9b3909fa9eb0db7f05fd36b5e6c96b06
Ioana Tamas <ioana@Ioanas-Laptop.local>
2024-03-11 20:22:26 -0400

    Rabin class

--- String/rabinquery.cpp
+++ String/rabinquery.cpp
@@ -1,25 +1,32 @@
-#define Nmax 101010
-#define p1 47
-#define p2 149
-#define MOD1 666013
-#define MOD2 991777
-int nr1[Nmax],pow1[Nmax],pow2[Nmax],nr2[Nmax],nrfin;
-char car[Nmax]; // string we want to hash, STARTING FROM 1
-int N,M;
-void make() {
-  pow1[0]=1,pow2[0]=1;
-  for(int i=1;i<=N;++i) {
-    pow1[i]=(1LL*pow1[i-1]*p1)%MOD1;
-    pow2[i]=(1LL*pow2[i-1]*p2)%MOD2;
-    nr1[i]=((1LL*nr1[i-1]*p1)%MOD1 + car[i])%MOD1;
-    nr2[i]=((1LL*nr2[i-1]*p2)%MOD2 + car[i])%MOD2;
+class RabinKarp {
+private:
+  vi nr[2], pw[2];
+  int p[2]= {47, 149};
+  int MD[2] = {666013, 991777};
+public:
+  RabinKarp(string &s) {
+    int N = sz(s);
+    FOR(k,2) {
+      nr[k].resize(N+1);
+      pw[k].resize(N+1);
+      pw[k][0] = 1;
+      nr[k][0] = 0;
+    }
+    for(int i=1;i<=N;++i) {
+      FOR(k,2) {
+        pw[k][i] = (1LL*pw[k][i-1] * p[k]) % MD[k];
+        nr[k][i] = (1LL*nr[k][i-1] * p[k] + s[i-1]) % MD[k];
+      }
+    }
   }
-}
-int querry(int x,int y,int x1,int y1,int debug) {
-  int sol1,sol2,sol12,sol22;
-  sol1=1LL*(nr1[y]-(1LL*pow1[y-x+1]*nr1[x-1])%MOD1+MOD1)%MOD1;
-  sol2=1LL*(nr1[y1]-(1LL*pow1[y1-x1+1]*nr1[x1-1])%MOD1+MOD1)%MOD1;
-  sol12=(nr2[y]-(1LL*pow2[y-x+1]*nr2[x-1])%MOD2+MOD2)%MOD2;
-  sol22=(nr2[y1]-(1LL*pow2[y1-x1+1]*nr2[x1-1])%MOD2+MOD2)%MOD2;
-  if(sol1==sol2 && sol12 == sol22) return 1; return 0;
-}
+  int eq(int x, int y, int x1, int y1) {
+    if(y-x != y1-x1) return 0;
+    ++x, ++y, ++x1, ++y1;
+    FOR(k, 2) {
+      int sol1 = (1LL*nr[k][y] - (1LL*pw[k][y-x+1]*nr[k][x-1]) % MD[k] + MD[k]) % MD[k];
+      int sol2 = (1LL*nr[k][y1] - (1LL*pw[k][y1-x1+1]*nr[k][x1-1]) % MD[k] + MD[k]) % MD[k];
+      if(sol1 != sol2) return 0;
+    }
+    return 1;
+  }
+};
