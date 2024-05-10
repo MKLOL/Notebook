61bff7671efe2d7fa369507dadc364586165f711
Dragos Ristache <ristachedragos@gmail.com>
2024-05-09 20:08:20 -0400

    Update dijkstra.cpp

--- Graph/dijkstra.cpp
+++ Graph/dijkstra.cpp
@@ -1,18 +1,42 @@
-int N, d[Nmax], viz[Nmax];
-vector<pii> g[Nmax];
-priority_queue<pii> pq;
-void dijkstra(int r) {
-  for(int i=1;i<=N;++i) d[i] = inf;
-  d[r] = 0; pq.push(mp(0,r));
-  while(!pq.empty()) {
-    int x = pq.top().sc; pq.pop(); viz[x] = 1;
-    for(auto a: g[x]) {
-      int y = a.fs, c = a.sc;
-      if(!viz[y]) {
-        if(d[y] > c + d[x]) {
-          d[y] = c + d[x]; pq.push(mp(-d[y],y));
+class Graph {
+
+  public:
+  int Nx;
+  vector<ll> d, viz;
+  vector<vector<pll>> g;
+
+  Graph(int n): Nx(n), d(n+1, inf), viz(n+1, 0), g(n+1) {}
+
+  void addEdge(int x, int y, ll cost) {
+    g[x].push_back({y, cost});
+  }
+
+  void runDijkstra(const vector<int>& s) {
+    priority_queue<pll, vector<pll>, greater<pll>> pq;
+    for (int i = 1; i <= Nx; ++i) d[i] = inf;
+    fill(viz.begin(), viz.end(), 0);
+
+    for (int x : s) {
+      d[x] = 0;
+      pq.push({0, x});
+    }
+
+    while (!pq.empty()) {
+      ll x = pq.top().second;
+      pq.pop();
+      if (viz[x]) continue;
+      viz[x] = 1;
+      
+      for (const auto& a : g[x]) {
+        ll y = a.first, c = a.second;
+        if (d[y] > d[x] + c) {
+            d[y] = d[x] + c;
+            pq.push({d[y], y});
         }
       }
     }
   }
-}
+  void runDijkstra(int s) {
+    runDijkstra({s});
+  }
+};
