9e9074759ddac2cf9de777d73d3fcb768709a1f9
Ioana Tamas <ioana@Ioanas-Laptop.local>
2024-06-10 21:58:02 +0300

    Make tree hash have 2 mods

--- Graph/TreeHash.cpp
+++ Graph/TreeHash.cpp
@@ -1,36 +1,38 @@
-// TODO: consider 2 mods
-
 class TreeHash {
   // assumes vi g global adjacency list
 public:
-  int hMOD;
-  vi dc; // constant added in hash product for each depth
+  int mods[2];
+  vi dc[2]; // constant added in hash product for each depth
   vi viz;
-  vi hashes; // stores final hashes
+  vi hashes[2]; // stores final hashes
 
   TreeHash() {}
   TreeHash(int N) {
-    hMOD = 1000000007;
+    mods[0] = 998244353;
+    mods[1] = 100000007;
     mt19937 rng(time(0));
-    uniform_int_distribution<mt19937::result_type> dist(1,hMOD);
+    uniform_int_distribution<mt19937::result_type> dist(1,mods[0]);
     viz.resize(N+1);
-    dc.resize(N+1);
-    hashes.resize(N+1);
+    FOR(k, 2) {
+      dc[k].resize(N+1);
+      hashes[k].resize(N+1);
+    }
     FOR(i, N+1) {
-      dc[i] = dist(rng);
+      FOR(k, 2) dc[k][i] = dist(rng);
       viz[i] = 0; 
     }
   } 
 
   void make_hash(int x, int d) {
     viz[x] = 1;
-    int hash = 1;
+    FOR(k, 2) hashes[k][x] = 1;
     for(auto y: g[x]) {
       if(viz[y]) continue;
       make_hash(y, d+1);
-      hash = 1LL * hash * (dc[d] + hashes[y]) % hMOD;
+      FOR(k, 2) {
+        hashes[k][x] = 1LL * hashes[k][x] * (dc[k][d] + hashes[k][y]) % mods[k];
+      }
     }
-    hashes[x] = hash;
   }
 
   void make_hashes(int r) {
