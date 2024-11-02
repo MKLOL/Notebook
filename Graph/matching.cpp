0bfc4b8c69be9cce3e7cd670237ef481549c3bbc
Ioana Tamas <timics23@gmail.com>
2024-11-01 20:27:48 -0400

    make matching classy

--- Graph/matching.cpp
+++ Graph/matching.cpp
@@ -1,19 +1,74 @@
-int N,M,L,R,K,v[Nmax],p;
-vi g[Nmax];
-int l[Nmax],r[Nmax],u[Nmax],was[Nmax],S;
-int match(int q) {
-  if(was[q]) return 0; was[q]=1;
-  for(auto x : g[q]) {
-    if(!r[x]) { l[q]=x; r[x]=q; return 1;}
+class Graph {
+public:
+  int L, R;
+  vvi g;
+  vi l, r, was;
+
+  // left nodes 0..L-1
+  // right nodes 0..R-1
+  // g has edges from L to R
+  Graph(int Lx, int Rx) {
+    L = Lx;
+    R = Rx;
+    clear();
   }
-  for(auto x: g[q]) {
-    if(match(r[x])) { l[q]=x; r[x]=q; return 1; }
+
+  void clear() {
+    l.resize(L);
+    r.resize(R);
+    was.resize(L);
+    g.resize(L);
+    FOR(i, L) {
+      l[i] = -1;
+      g[i].clear();
+    }
+    FOR(i, R) {
+      r[i] = -1;
+    }
   }
-  return 0;
-}
-void matching() { //edges i,l[i] if l[i]>0
-  int ok = 1; while(ok) {
-    ok = 0; for(int i=0;i<=L;++i) was[i]=0;
-    for(int i=1;i<=L;++i) { if(!l[i]) ok|= match(i); }
+
+  void addEdge(int x, int y) {
+    g[x].pb(y);
   }
-}
+
+  int match(int q) {
+    if(was[q]) return 0; 
+    was[q] = 1;
+
+    for(auto x : g[q]) {
+      if(r[x] == -1) {
+        l[q] = x;
+        r[x] = q;
+        return 1;
+      }
+    }
+
+    for(auto x: g[q]) {
+      if(r[x] != -1 && match(r[x])) {
+        l[q] = x;
+        r[x] = q;
+        return 1;
+      }
+    }
+    return 0;
+  }
+
+  //edges i, l[i] if l[i] != -1
+  vpi matching() { 
+    int ok = 1; 
+    while(ok) {
+      ok = 0; 
+      FOR(i, L) was[i]=0;
+
+      FOR(i, L) {
+        if(l[i] == -1) ok |= match(i);
+      }
+
+    }
+    vpi ret;
+    FOR(i, L) {
+      if(l[i] != -1) ret.pb(mp(i, l[i]));
+    }
+    return ret;
+  }
+};
