9091eeb240a391f26c71109aa53a4244b345d9fd
Dragos Ristache <ristachedragos@gmail.com>
2025-01-26 16:15:05 -0500

    Update kmp.cpp

--- String/kmp.cpp
+++ String/kmp.cpp
@@ -1,27 +1,28 @@
-class Kmp {
-public:
-  vi nx;
-  string p; //pattern
-
-  Kmp(string &s) p(s) {
-    nx.resize(sz(s));
-    int k = -1; nx[0] = -1;
-    rep(i,1,sz(s)) {
-      while(k >= 0 && s[k+1] != s[i]) k = nx[k];
-      if(s[k+1] == s[i]) ++k;
+namespace kmp {
+  template<typename T>
+  vector<int> build_nx(const T &s1) {
+    vector<int> nx(s1.size());
+    int k = -1;
+    nx[0] = -1;
+    for (size_t i = 1; i < s1.size(); ++i) {
+      while (k >= 0 && s1[k + 1] != s1[i]) k = nx[k];
+      if (s1[k + 1] == s1[i]) ++k;
       nx[i] = k;
-    } 
+    }
+    return nx;
   }
 
-  vi match(string &s) {
-    vi ret;
-    int k=-1;
-    for(int i=0;i<sz(s);++i) {
-      while(k >= 0 && p[k+1] != s[i]) k = nx[k];
-      if(p[k+1] == s[i]) ++k; 
-      if(k==sz(p)-1) {
+  template<typename T>
+  vector<int> match(const T &s1, const T &s2) {
+    vector<int> nx = build_nx(s1);
+    vector<int> ret;
+    int k = -1;
+    for (size_t i = 0; i < s2.size(); ++i) {
+      while (k >= 0 && s1[k + 1] != s2[i]) k = nx[k];
+      if (s1[k + 1] == s2[i]) ++k;
+      if (k == static_cast<int>(s1.size()) - 1) {
+        ret.push_back(static_cast<int>(i - s1.size() + 1));
         k = nx[k];
-        ret.pb(i-sz(p)+1);
       }
     }
     return ret;
